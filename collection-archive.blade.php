97fd6da1b0a9c4c53facaeea5ebc4d1bcab1e623
Nono Martínez Alonso <mundowarezweb@gmail.com>
2019-02-19 00:08:03 +0100

    Update collection-archive.blade.php

--- collection-archive.blade.php
+++ collection-archive.blade.php
@@ -2,31 +2,44 @@
 
 <?php
 	$header_classes = [];
-    $hide_date = false;
- 	if(isset($item)) {
-    	$site_title = $item->title.' · '.config('folio.title');
+	$hide_date = false;
+	if(isset($item)) {
+		$site_title = $item->title.' · '.config('folio.title');
 		$collection = $item->collection();
-        $hide_date = $item->boolProperty('hide-date', $hide_date);
-  	}
+		$hide_date = $item->boolProperty('hide-date', $hide_date);
+	}
 ?>
 
 @section('content')
 
-		<div class="[ o-wrap o-wrap--size-standard ]">
+	<div class="[ o-wrap o-wrap--size-standard ]">
 
-		  	@isset($items_expected))
-				@foreach($items_expected as $i)
-					{!! view('folio::partial.c-item-li')->with(['item' => $i, 'expected' => true]) !!}
-				@endforeach
-			@endisset
 
-			@foreach($collection as $i)
-				{!! view('folio::partial.c-item-li')->with(['item' => $i]) !!}
+		@isset($items_expected))
+		
+			@foreach($items_expected as $i)
+			
+				{!! view('folio::partial.c-item-li')->with(['item' => $i, 'expected' => true]) !!}
+				
 			@endforeach
+			
+		@endisset
 
-			@if(isset($ids))
-				{!! view('folio::partial.c-load-more')->with(['ids' => $ids]) !!}
-			@endif
-		</div>
+
+		@foreach($collection as $i)
+		
+			{!! view('folio::partial.c-item-li')->with(['item' => $i]) !!}
+			
+		@endforeach
+
+
+		@if(isset($ids))
+		
+			{!! view('folio::partial.c-load-more')->with(['ids' => $ids]) !!}
+			
+		@endif
+
+
+	</div>
 
 @stop
